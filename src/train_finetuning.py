e219e8718eaed6f233eea36346f3e9cb3d0170b0
Shyam <beingprofess@gmail.com>
2019-01-10 13:22:46 +0530

    added separate files of bottleneck and finetuning

--- src/train_finetuning.py
+++ src/train_finetuning.py
@@ -29,8 +29,6 @@ import glob
 import os
 from PIL import Image
 
-
-
 def get_num_classes_column_lb(column_name, df, headings_dict):
 
     # use for getting number of predictions for multi class classification
@@ -83,9 +81,9 @@ ntrain = df_train.shape[0]
 print('ntrain')
 nvalid = df_validation.shape[0]
 batch_size = 4
-epochs_first = 15 # For bottleneck training
+epochs_first = 5 # For bottleneck training
 epochs_second = 30 # For Fine tuning
-target_size = (244, 244)
+target_size = (224, 224)
 
 nbatches_train, mod = divmod(ntrain, batch_size)
 nbatches_valid, mod = divmod(nvalid, batch_size)
@@ -97,9 +95,9 @@ parametrization_dict = {'multi_class':[{'L1_output':'id'}],'multi_label':[]}
 train_generator = generator_from_df(df_train, df_overall, headings_dict, batch_size, target_size, features=None, parametrization_dict = parametrization_dict)
 validation_generator = generator_from_df(df_validation, df_overall, headings_dict, batch_size, target_size, features=None, parametrization_dict= parametrization_dict)
 
-print("Downloading Base Model.....")
-base_model = InceptionV3(weights='imagenet', include_top=False)
-
+filepath1 = fine_tuned_checkpoint_path + experiment_name + "_inceptionv3_bottleneck_{epoch:02d}_{val_acc:.2f}.h5"
+#Save the model after every epoch.
+mc_fit = ModelCheckpoint(filepath1, monitor='val_acc', verbose=0, save_best_only=True, save_weights_only=False, mode='auto', period=1)
 
 # add a global spatial average pooling layer
 x = base_model.output
@@ -113,34 +111,36 @@ predictions = Dense(46, activation='softmax')(x)
 # this is the model we will train
 model = Model(input=base_model.input, output=predictions)
 
-if os.path.exists(top_layers_checkpoint_path):
-	model.load_weights(top_layers_checkpoint_path)
+if os.path.exists(fine_tuned_checkpoint_path):
+	model.load_weights(fine_tuned_checkpoint_path)
 	print ("Checkpoint '" + top_layers_checkpoint_path + "' loaded.")
 
-# first: train only the top layers (which were randomly initialized)
-# i.e. freeze all convolutional InceptionV3 layers
-for layer in base_model.layers:
-    layer.trainable = False
 
-# compile the model (should be done *after* setting layers to non-trainable)
+# we chose to train the top 2 inception blocks, i.e. we will freeze
+# the first 172 layers and unfreeze the rest:
+for layer in model.layers[:172]:
+   layer.trainable = False
+for layer in model.layers[172:]:
+   layer.trainable = True
 
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
-##############################y code
 
-filepath= top_layers_checkpoint_path + experiment_name + "_inceptionv3_bottleneck_{epoch:02d}_{val_acc:.2f}.h5"
-##############################y code
-#Save the model after every epoch.
-mc_top = ModelCheckpoint(filepath, monitor='val_acc', verbose=0, save_best_only=True, save_weights_only=False, mode='auto', period=1)
-callbacks_list = [checkpoint]
+# we need to recompile the model for these modifications to take effect
+# we use SGD with a low learning rate
+from keras.optimizers import SGD
+model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy', metrics=['accuracy'])
+
+# we train our model again (this time fine-tuning the top 2 inception blocks
+# alongside the top Dense layers
+#model.fit_generator(...)
 
 model.fit_generator(
     generator=train_generator,
     steps_per_epoch=nbatches_train,
-    epochs=epochs_first,
+    epochs=epochs_second,
     verbose=1,
     callbacks=callbacks_list,
     validation_data=validation_generator,
     validation_steps=nbatches_valid,
-    workers=nworkers)
+    workers=[mc_fit])
 
-model.save(bottleneck.h5)
+model.save()
