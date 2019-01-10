f9780b6f497235f81ef9d71d856c2c5e740341c2
Shyam <beingprofess@gmail.com>
2019-01-11 00:59:08 +0530

    changed loss functions

--- src/train_finetuning.py
+++ src/train_finetuning.py
@@ -76,7 +76,7 @@ for layer in model.layers[172:]:
 # we need to recompile the model for these modifications to take effect
 # we use SGD with a low learning rate
 from keras.optimizers import SGD
-model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 # we train our model again (this time fine-tuning the top 2 inception blocks
 # alongside the top Dense layers
