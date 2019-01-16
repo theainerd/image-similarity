ff068ad5529db55a4463dd56ced644ad9cd8ccdb
Shyam <beingprofess@gmail.com>
2019-01-16 19:28:20 +0530

    vector extract

--- src/extract_features.py
+++ src/extract_features.py
@@ -61,7 +61,6 @@ def extract_vector(image_path):
     # preds = intermediate_layer_model.predict(image)
     preds = base_model.predict(image)
     preds = preds[0][0][0]
-    preds = preds.tolist()
     print("Extracting Image"+image_path)
     print(preds)
     return preds
