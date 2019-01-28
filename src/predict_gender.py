451d28d8234e67ab7f3b1771deacf616aaea61b8
Shyam <beingprofess@gmail.com>
2019-01-28 12:05:43 +0530

    gender dataset

--- src/predict_gender.py
+++ src/predict_gender.py
@@ -57,64 +57,65 @@ validate_images = True
 traindf = pd.read_csv("../data/gender_balanced.csv")
 traindf = traindf[['_id','gender']]
 no_of_classes = len(traindf['gender'].unique())
-class_weight = class_weight.compute_class_weight('balanced',
-                                                 np.unique(traindf['gender']),
-                                                 traindf['gender'])
+print(no_of_classes)
+# class_weight = class_weight.compute_class_weight('balanced',
+#                                                  np.unique(traindf['gender']),
+#                                                  traindf['gender'])
 
-datagen = ImageDataGenerator(
-        rotation_range=40,
-        width_shift_range=0.2,
-        height_shift_range=0.2,
-        rescale=1./255,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True,
-        fill_mode='nearest')
+# datagen = ImageDataGenerator(
+#         rotation_range=40,
+#         width_shift_range=0.2,
+#         height_shift_range=0.2,
+#         rescale=1./255,
+#         shear_range=0.2,
+#         zoom_range=0.2,
+#         horizontal_flip=True,
+#         fill_mode='nearest')
 
-test_datagen = ImageDataGenerator(rescale=1./255)
+# test_datagen = ImageDataGenerator(rescale=1./255)
 
-train_generator = datagen.flow_from_directory(
-	train_data_dir,
-	target_size=(img_width, img_height),
-	batch_size=batch_size,
-	class_mode="categorical",
-	shuffle=True)
+# train_generator = datagen.flow_from_directory(
+# 	train_data_dir,
+# 	target_size=(img_width, img_height),
+# 	batch_size=batch_size,
+# 	class_mode="categorical",
+# 	shuffle=True)
 
-validation_generator = test_datagen.flow_from_directory(
-	validation_data_dir,
-	target_size=(img_width, img_height),
-	batch_size=batch_size,
-	class_mode="categorical",
-	shuffle=True)
+# validation_generator = test_datagen.flow_from_directory(
+# 	validation_data_dir,
+# 	target_size=(img_width, img_height),
+# 	batch_size=batch_size,
+# 	class_mode="categorical",
+# 	shuffle=True)
 
-print("Downloading Base Model.....")
+# print("Downloading Base Model.....")
 
-base_model = InceptionV3(weights='imagenet', include_top=False)
+# base_model = InceptionV3(weights='imagenet', include_top=False)
 
-# gender attribute layer
+# # gender attribute layer
 
-gender_attribute = base_model.output
-gender_attribute = GlobalAveragePooling2D(name = 'global_average_pooling2d_3')(gender_attribute)
-gender_attribute = Dropout(dropout)(gender_attribute)
-# let's add a fully-connected layer
-gender_attribute_layer = Dense(1024, activation='relu',name = "attribute_gender")(gender_attribute)
-predictions_gender = Dense(no_of_classes,activation = 'softmax',name="predictions_gender")(gender_attribute_layer)
+# gender_attribute = base_model.output
+# gender_attribute = GlobalAveragePooling2D(name = 'global_average_pooling2d_3')(gender_attribute)
+# gender_attribute = Dropout(dropout)(gender_attribute)
+# # let's add a fully-connected layer
+# gender_attribute_layer = Dense(1024, activation='relu',name = "attribute_gender")(gender_attribute)
+# predictions_gender = Dense(no_of_classes,activation = 'softmax',name="predictions_gender")(gender_attribute_layer)
 
-model = Model(inputs=base_model.input, outputs = predictions_gender)
+# model = Model(inputs=base_model.input, outputs = predictions_gender)
 
-# model.load_weights("../models/label_gender/label_gender_inceptionv3_41_0.37.h5")
-# print ("Checkpoint loaded.")
+# # model.load_weights("../models/label_gender/label_gender_inceptionv3_41_0.37.h5")
+# # print ("Checkpoint loaded.")
 
-# change this code for every attribute - set the layers to true for training
-for layer in base_model.layers:
-    layer.trainable = False
+# # change this code for every attribute - set the layers to true for training
+# for layer in base_model.layers:
+#     layer.trainable = False
 
-# this is the model we will train
+# # this is the model we will train
 
-model.compile(optimizer = Adam(0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
+# model.compile(optimizer = Adam(0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
 
-filepath= output_models_dir + experiment_name + "_inceptionv3_{epoch:02d}_{val_acc:.2f}.h5"
-checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=False, save_weights_only=False, mode='auto', period=1)
-checkpoints =[checkpoint]
-model.fit_generator(train_generator, epochs = epochs, validation_data=validation_generator,class_weight = class_weight, callbacks=checkpoints)
-model.save(final_model_name)
+# filepath= output_models_dir + experiment_name + "_inceptionv3_{epoch:02d}_{val_acc:.2f}.h5"
+# checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=False, save_weights_only=False, mode='auto', period=1)
+# checkpoints =[checkpoint]
+# model.fit_generator(train_generator, epochs = epochs, validation_data=validation_generator,class_weight = class_weight, callbacks=checkpoints)
+# model.save(final_model_name)
