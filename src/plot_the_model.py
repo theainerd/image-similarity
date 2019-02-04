cd28488c51f3b6671df984d68639beb3e5034fa2
Shyam <beingprofess@gmail.com>
2019-02-04 14:30:17 +0530

    visualize

--- src/plot_the_model.py
+++ src/plot_the_model.py
@@ -1,4 +1,4 @@
-from keras.models import load_model
+from keras.models import load_model,
 from keras.models import Model
 from keras.utils import plot_model
 
@@ -10,7 +10,7 @@ model_pattern = load_model("../models/finalmodel/label_pattern_inceptionv3_50_0.
 final_attribute = final_model.get_layer('mixed5').output
 final_attribute = model_pattern.get_layer('conv2d_55')(final_attribute)
 
-for i,layer in final_model.layers:
+for i,layer in enumerate(final_model.layers):
     weights = layer.get_weights()
     print(i,weights)
 
