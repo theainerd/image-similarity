c21dc039998b6d10b68d1fdd0a0e438b2887a716
Shyam <beingprofess@gmail.com>
2019-02-04 14:27:01 +0530

    visualize

--- src/plot_the_model.py
+++ src/plot_the_model.py
@@ -11,5 +11,9 @@ final_attribute = final_model.get_layer('mixed5').output
 final_attribute = model_pattern.get_layer('conv2d_55')(final_attribute)
 print(final_attribute.get_weights())
 
+for i,layer in final_model.layers:
+    weights = layer.get_weights()
+    print(i,weights)
+
 pattern_attribute = final_model.get_layer('mixed5').output
 print(pattern_attribute.get_weights())
