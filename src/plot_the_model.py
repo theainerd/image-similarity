11beccb954ac60185e421dd3ed9a83e246efa77c
Shyam <beingprofess@gmail.com>
2019-02-04 16:06:13 +0530

    visualize

--- src/plot_the_model.py
+++ src/plot_the_model.py
@@ -8,18 +8,18 @@ print("Loading  model.\n")
 final_model = load_model("../models/final_model.h5")
 model_pattern = load_model("../models/finalmodel/label_pattern_inceptionv3_50_0.54.h5")
 
-# for i,layer in enumerate(final_model.layers):
-#     print(i,layer.name)
+for i,layer in enumerate(final_model.layers):
+    print(i,layer.name)
 
 
-# for i,layer in enumerate(model_pattern.layers):
-#     print(i,layer.name)
+for i,layer in enumerate(model_pattern.layers):
+    print(i,layer.name)
 
 
-for i,layer in enumerate(final_model.layers[600:603]):
-    weights = layer.get_weights()
-    print(i,weights)
+# for i,layer in enumerate(final_model.layers[600:603]):
+#     weights = layer.get_weights()
+#     print(i,weights)
 
-for i,layer1 in enumerate(model_pattern.layers[313:]):
-    weights1 = layer1.get_weights()
-    print(i,weights1)
+# for i,layer1 in enumerate(model_pattern.layers[313:]):
+#     weights1 = layer1.get_weights()
+#     print(i,weights1)
