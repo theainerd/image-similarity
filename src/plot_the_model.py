28d8b995f7d9810ffa8a985199e3eecc71893e99
Shyam <beingprofess@gmail.com>
2019-02-04 13:58:25 +0530

    visualize

---
+++ src/plot_the_model.py
@@ -0,0 +1,10 @@
+from keras.models import load_model
+from keras.models import Model
+from keras.utils import plot_model
+
+print("Loading color model.\n")
+final_model = load_model("../models/final_model.h5")
+# model_pattern = load_model("../models/finalmodel/label_pattern_inceptionv3_50_0.54.h5")
+
+pattern_attribute = model_pattern.get_layer('mixed5')
+print(pattern_attribute.get_weights())
