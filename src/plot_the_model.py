0d8dd4756324fef5e7c2ad77692f0ff2714b3e0b
Shyam <beingprofess@gmail.com>
2019-02-04 13:59:34 +0530

    visualize

--- src/plot_the_model.py
+++ src/plot_the_model.py
@@ -6,5 +6,5 @@ print("Loading color model.\n")
 final_model = load_model("../models/final_model.h5")
 # model_pattern = load_model("../models/finalmodel/label_pattern_inceptionv3_50_0.54.h5")
 
-pattern_attribute = model_pattern.get_layer('mixed5')
+pattern_attribute = final_model.get_layer('mixed5')
 print(pattern_attribute.get_weights())
