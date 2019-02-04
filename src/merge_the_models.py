f4bdcdd86886b748a1285ba435e7bcbf20cd48c5
Shyam <beingprofess@gmail.com>
2019-02-04 22:21:42 +0530

    new data

--- src/merge_the_models.py
+++ src/merge_the_models.py
@@ -1,8 +1,12 @@
 from keras.models import load_model
 from keras.models import Model
 
-model_pattern = load_model("../models/label_pattern/label_pattern_inceptionv3_10_0.15.h5")
-model_color = load_model("../models/label_pattern/multiclass_02_0.10.h5")
+model_pattern = load_model("../models/label_pattern_bottleneck/label_pattern_inceptionv3_03_0.39.h5")
+model_color = load_model("../models/label_color_bottleneck/label_color_inceptionv3_05_0.22.h5")
+
+for layer in model_color.layers:
+    layer.name = layer.name + str("_color")
+
 
 pattern_attribute = model_pattern.get_layer('mixed10').output
 pattern_attribute = model_pattern.get_layer('global_average_pooling2d_1')(pattern_attribute)
@@ -15,11 +19,11 @@ predictions_pattern = model_pattern.get_layer('predictions_pattern')(pattern_att
 # color_attribute = model.get_layer('attribute_color')(color_attribute)
 # color_attribute = model.get_layer('predictions_color')(color_attribute)
 
-color_attribute = model_pattern.get_layer('mixed10').output
-color_attribute = model_color.get_layer('global_average_pooling2d_2')(color_attribute)
-color_attribute = model_color.get_layer('dropout_2')(color_attribute)
-color_attribute = model_color.get_layer('attribute_color')(color_attribute)
-predictions_color = model_color.get_layer('predictions_color')(color_attribute)
+color_attribute = model_pattern.get_layer('mixed10_color').output
+color_attribute = model_color.get_layer('global_average_pooling2d_1_color')(color_attribute)
+color_attribute = model_color.get_layer('dropout_2_color')(color_attribute)
+color_attribute = model_color.get_layer('attribute_color_color')(color_attribute)
+predictions_color = model_color.get_layer('predictions_color_color')(color_attribute)
 
 final_model = Model(inputs= model_pattern.input, outputs= [predictions_pattern,predictions_color])
 
