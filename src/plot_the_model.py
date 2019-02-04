c2e7b4e67d146bbe4dbc0755b57ff94dc93c68d3
Shyam <beingprofess@gmail.com>
2019-02-04 14:14:06 +0530

    visualize

--- src/plot_the_model.py
+++ src/plot_the_model.py
@@ -8,7 +8,7 @@ final_model = load_model("../models/final_model.h5")
 model_pattern = load_model("../models/finalmodel/label_pattern_inceptionv3_50_0.54.h5")
 
 final_attribute = final_model.get_layer('mixed5').get_weights()
-print(final_attribute.)
+print(final_attribute)
 
 pattern_attribute = final_model.get_layer('mixed5').output
 print(pattern_attribute.get_weights())
