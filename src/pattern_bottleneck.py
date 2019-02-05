726b795aa40e20ae915e43332072bff94c5f66c6
Shyam <beingprofess@gmail.com>
2019-02-05 12:15:37 +0530

    pattern_resnet

--- src/pattern_bottleneck.py
+++ src/pattern_bottleneck.py
@@ -13,6 +13,7 @@ from keras.optimizers import SGD
 from keras.models import Sequential
 from keras.layers import Dropout, Flatten, Dense, GlobalAveragePooling2D
 from keras.applications.inception_v3 import InceptionV3
+from keras.applications.resnet50 import ResNet50
 from keras.applications.vgg16 import VGG16
 from keras.regularizers import l2
 from keras.applications.resnet50 import preprocess_input
@@ -40,7 +41,7 @@ import pandas as pd
 
 
 #configurations
-no_of_classes = 14
+no_of_classes = 6
 epochs = 50
 batch_size = 32
 dropout = 0.5
@@ -196,7 +197,7 @@ print("Downloading Base Model.....")
 
 K.set_learning_phase(0)
 
-base_model = InceptionV3(include_top=False, weights='imagenet')
+base_model = ResNet50(include_top=False, weights='imagenet')
 
 for layer in base_model.layers:
     layer.trainable = False
--- src/plot_the_model.py
+++ src/plot_the_model.py
@@ -5,13 +5,8 @@ from keras import layers
 
 print("Loading  model.\n")
 
-final_model = load_model("../models/final_model.h5")
-model_pattern = load_model("../models/finalmodel/label_pattern_inceptionv3_50_0.54.h5")
+model = load_model("../models/model_12_128.h5")
+# model_pattern = load_model("../models/finalmodel/label_pattern_inceptionv3_50_0.54.h5")
 
-for i,layer in enumerate(final_model.layers[570:]):
-    weights = layer.get_weights()
-    print(i,layer.name,weights)
-
-for i,layer1 in enumerate(model_pattern.layers[300:]):
-    weights1 = layer1.get_weights()
-    print(i,layer1.name,weights1)
+from keras.utils import plot_model
+plot_model(model, to_file='model_yash.png')
