d07a08f9611982ce7709a63808d5eeec89f08e1f
Shyam <beingprofess@gmail.com>
2019-02-05 14:38:15 +0530

    pattern_resnet

--- src/predict_pattern.py
+++ src/predict_pattern.py
@@ -51,7 +51,7 @@ validation_data_dir = data_dir + 'validation'
 experiment_name = "label_pattern"
 img_width, img_height = 299, 299
 original_img_width, original_img_height = 400, 400
-final_model_name = experiment_name + '_inceptionv3_bottleneck_final.h5'
+final_model_name = experiment_name + '_inceptionv3_new_bottleneck_final.h5'
 validate_images = True
 
 traindf = pd.read_csv("../data/pattern_balanced.csv")
