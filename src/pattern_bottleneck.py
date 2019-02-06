485cb4aa3277009689b6de592d0db792ece07977
Shyam <beingprofess@gmail.com>
2019-02-06 19:36:35 +0530

    attention module

--- src/pattern_bottleneck.py
+++ src/pattern_bottleneck.py
@@ -54,7 +54,7 @@ data_dir = "../data/pattern_balanced_split/"
 output_models_dir = "../models/label_pattern_bottleneck/"
 train_data_dir  = data_dir + 'train'
 validation_data_dir = data_dir + 'validation'
-experiment_name = "label_pattern_attention_inception"
+experiment_name = "label_pattern_attention_xception"
 img_width, img_height = 331,331
 original_img_width, original_img_height = 400, 400
 final_model_name = experiment_name + '_xception_bottleneck.h5'
@@ -188,7 +188,7 @@ print(class_weight)
 
 print("Downloading Base Model.....")
 
-base_model = InceptionV3(include_top=False, weights='imagenet')
+base_model = Xception(include_top=False, weights='imagenet')
 
 for layer in base_model.layers:
     layer.trainable = False
